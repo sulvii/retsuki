9228742d8acffe1c89fdec2cc362980c1869462f
Mohtasim Alam <94981761+sulvii@users.noreply.github.com>
2026-06-17 15:06:34 +0600

    try watcher ig

---
+++ watcher.ts
@@ -0,0 +1,9 @@
+import { Watcher, type WatcherOptions } from '@slipher/watcher'
+
+// @ts-expect-error
+const watcherOptions: WatcherOptions = {
+    srcPath: './src',
+    debug: true,
+}
+
+export const watcher = new Watcher(watcherOptions)
